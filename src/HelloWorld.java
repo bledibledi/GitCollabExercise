b0cd5abce263ee88d275c81fd9a5e7f6db632e6b
Faruk Hesenjan <fh522@vt.edu>
2021-09-01 09:51:53 -0400

    Added Operators

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("bledi");
     }
+
+    public void printNum() {
+        System.out.println("1,2,3");
+    }
+    public void operators(){
+        System.out.println("+,-,*,/");
+    }
 }
