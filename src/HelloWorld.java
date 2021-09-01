101f97dc95094643de89e1d6a21232fd2c28a55d
bledi <bledi.bllaca12@gmail.com>
2021-09-01 15:36:31 +0200

    added my new method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("bledi");
     }
+
+    public void printNums(){
+        System.out.println("1,2,3");
+    }
 }
