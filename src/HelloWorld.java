cf753e6d765ed802d2af6de28fcb4299ac0b8ad4
ViktoriiaMoskul <moskul1v@gmail.com>
2021-09-01 09:50:58 -0400

    Viktoriia added loop

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("bledi");
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }//Viktoriia
+
+
     }
 }
